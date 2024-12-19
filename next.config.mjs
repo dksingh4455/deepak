98431e13670843d1a3541dd865c99fb6dfa1d384
dksingh4455 <gitinsuri982@gmail.com>
2024-12-19 19:43:27 +0530

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export', // Ensures the app is exported as a static site
-    trailingSlash: true, // Adds trailing slashes to URLs for static routing
-    images: {
-      unoptimized: true, // Avoids issues with Next.js image optimization
-    },
-  };
-  
-  export default nextConfig;
+const nextConfig = {};
+
+export default nextConfig;
+
   
