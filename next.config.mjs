f0e43941d961b6550d9fc17f6a22e039a87d8f28
dksingh4455 <gitinsuri982@gmail.com>
2024-12-19 19:52:24 +0530

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const path = require('path');
 
-export default nextConfig;
+module.exports = {
+  webpack(config) {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+  },
+};
+
 
   
