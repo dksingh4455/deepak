438603179d30b83897729d22aa392c71c4c3209e
dksingh4455 <gitinsuri982@gmail.com>
2024-12-20 12:25:20 +0530

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true, // Enable Next.js's SWC-based minifier
+  experimental: {
+    swcMinify: true, // Explicitly enable SWC minification
+  },
 };
 
 export default nextConfig;
+
