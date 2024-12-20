92cb190c51c4ba08499ef67a7dad38d5e187ab5a
dksingh4455 <gitinsuri982@gmail.com>
2024-12-20 13:42:58 +0530

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    swcMinify: true, // Explicitly enable SWC minification
-  },
 };
 
 export default nextConfig;
+
 
